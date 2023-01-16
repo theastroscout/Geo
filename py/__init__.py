7f387fcd8b00e5242cf618b0c8138c9c12a73cf2
Alexander Yermolenko <i@surfy.one>
2023-01-16 21:37:11 +0500

    Added Timezone for Python

--- py/__init__.py
+++ py/__init__.py
@@ -13,10 +13,41 @@ class Geo():
 			'Content-type': 'application/json'
 		}
 
+	'''
+	
+	Get location by IP
+
+	'''
+
 	def ip(self, IP):
 		url = 'https://api.surfy.one/geo/ip/' + IP
 		result = requests.post(url, headers = self.headers)
 		return result.json()
+	
+	'''
+
+	Get Timezone by coordinates
+
+	'''
+
+	def tz(self, coords):
+		url = 'https://api.surfy.one/geo/timezone'
+		data = {
+			'lat': coords[0],
+			'lng': coords[1]
+		}
+		
+		try:
+			result = requests.post(url, json=data, headers = self.headers)
+			result = result.json()
+		except:
+			result = {'state': False}
+
+	'''
+	
+	Reverse GeoCoding
+
+	'''
 
 	def reverse(self, coords):
 		url = 'https://api.surfy.one/geo/reverse'
